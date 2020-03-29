4988201ddbecaf81c6f7a4d6df709aa0a0b3a2d4
helloimiskra <ibatistapoblete@gmail.com>
2020-03-29 19:26:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+
+function submitData(name, email){
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({name, email})
+  };
+  return fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+          return response.json();
+     })
+     .then(function(object) {
+       document.body.innerHTML = `<h1> ${object.id}</h1>`;
+     })
+     .catch(function(error) {
+        document.body.innerHTML = `<h1> ${error.message} </h1>`;
+       console.log(error.message);
+      });
+    };
